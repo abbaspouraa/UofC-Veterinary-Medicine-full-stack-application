215b3180c0fd943f699c0a3c1308644c38137b55
Ali Zirahi <ali.zirahi@ucalgary.ca>
2021-12-11 19:07:12 -0700

    Update TreatmentProcess.jsx

--- Hackton and project FrontEnd/src/components/Ali/TreatmentProcess.jsx
+++ Hackton and project FrontEnd/src/components/Ali/TreatmentProcess.jsx
@@ -13,14 +13,13 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import TreatmentService from './service/TreatmentService';
 
-export default function TreatmentProcess({animalName}) {
+export default function TreatmentProcess({animalId, token}) {
 
-
-    const [careattid, setCareAttId] = useState('');
-    // const [animalName, setAnimalName] = useState('');
-    const [date, setDate] = useState('');
-    const [processDescription, setProcessDescription] = useState('');
-    const [temperature, setTemperature] = useState('');
+    // const [statusid, setStatusIs]= useState(null);
+    // const [careattid, setCareAttId] = useState('');
+    // const [date, setDate] = useState('');
+    const [briefDescription, setBriefDescription] = useState('');
+    const [temp, setTemp] = useState('');
     const [weight, setWeight] = useState('');
     const [heartRate, setHeartRate] = useState('');
     const [symptoms, setSymptoms] = useState('');
@@ -29,12 +28,22 @@ export default function TreatmentProcess({animalName}) {
     //const [] = useState([]);
 
 
-    const startTreatment = () => {
+    const startTreatment = (e) => {
+        e.preventDefault();
 
-        TreatmentService.startNewTreatment(careattid,animalName,date,processDescription,temperature,weight,
-            heartRate,symptoms,diagnoseDrug,vetid).catch(error =>{
-            console.log(error);
-        })
+        TreatmentService.startNewTreatment({
+        
+            stage: 'started',
+            careattid: token.UCID,
+            animalid: animalId,
+            processDescription: briefDescription,
+            temperature: temp,
+            weight: weight,
+            heartRate: heartRate,
+            symptoms: symptoms,
+            diagnoseDrug: diagnoseDrug,
+            vetid: vetid
+        }).then(r => {console.log(r);})
     }
 
     // const reserveAnimal = (id) => {
@@ -59,31 +68,46 @@ export default function TreatmentProcess({animalName}) {
             {/* <TextField id="outlined-basic" label="Animal ID" variant="outlined" value={fname}
                             onChange={(e) => setFName(e.target.value)} /> */}
 
-            <h1>test: {animalName}</h1>
+            <h1>test: {animalId}</h1>
+
+            <h2> Care Att: {token.UCID}</h2>
             <h3>---  To be filled By Care Att. --- </h3>
-            <TextField id="outlined-basic" label="Care Attendent ID" variant="outlined" value={careattid}
-                            onChange={(e) => setCareAttId(e.target.value)}  />
-            <TextField id="outlined-basic" label="Date" variant="outlined" value={date}
-                            onChange={(e) => setDate(e.target.value)} />
-            <TextField id="outlined-basic" label="Problem Description" variant="outlined" value={processDescription}
-                            onChange={(e) => setProcessDescription(e.target.value)} />
-            <TextField id="outlined-basic" label="Temperature" variant="outlined" value={temperature}
-                            onChange={(e) => setTemperature(e.target.value)} />
+            {/* <TextField id="outlined-basic" label="Care Attendent ID" variant="outlined" value={careattid}
+                            onChange={(e) => setCareAttId(e.target.value)}  /> */}
+            {/* <TextField id="outlined-basic" label="Date" variant="outlined" value={date}
+                            onChange={(e) => setDate(e.target.value)} /> */}
+            <TextField id="outlined-multiline-static" label="Problem Description" variant="outlined" multiline 
+                    maxRows={4} value={briefDescription}
+                            onChange={(e) => setBriefDescription(e.target.value)} />
+            <TextField id="outlined-basic" label="Temperature" variant="outlined" value={temp}
+                            onChange={(e) => setTemp(e.target.value)} />
             <TextField id="outlined-basic" label="Weight" variant="outlined" value={weight}
                             onChange={(e) => setWeight(e.target.value)} />
             <TextField id="outlined-basic" label="Heart Rate" variant="outlined" value={heartRate}
                             onChange={(e) => setHeartRate(e.target.value)} />
-            <TextField id="outlined-basic" label="Symptoms" variant="outlined" value={symptoms}
+            <TextField id="outlined-multiline-static" label="Symptoms" variant="outlined" multiline 
+                    maxRows={4} value={symptoms}
                             onChange={(e) => setSymptoms(e.target.value)} />
-            <h3>---  To be filled By Vet --- </h3>
+            {/* <h3>---  To be filled By Vet --- </h3> */}
 
-            <TextField id="outlined-basic" label="Symptoms" variant="outlined" value={diagnoseDrug}
-                            onChange={(e) => setDiagnoseDrug(e.target.value)} />
-            <TextField id="outlined-basic" label="Symptoms" variant="outlined" value={vetid}
+            {/* <TextField id="outlined-basic" label="Diagnosed Drug" variant="outlined" value={diagnoseDrug}
+                            onChange={(e) => setDiagnoseDrug(e.target.value)} /> */}
+            {/* <TextField id="outlined-basic" label="Vet ID" variant="outlined" value={vetid}
                             onChange={(e) => setVetId(e.target.value)} />
 
+
+            <TextField
+                    id="outlined-multiline-static"
+                    label="Diagnosed Drug"
+                    multiline 
+                    maxRows={4}
+                    value={diagnoseDrug}
+                    onChange={(e) => setDiagnoseDrug(e.target.value)}
+                    variant="outlined"
+                    /> */}
+
             <Button size="small" variant="contained"
-                        onClick={startTreatment}
+                        onClick={(e) => startTreatment(e)}
             >Submit</Button>
 
         </Box>
